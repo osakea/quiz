27df3208b12d3c1d4d1829fd38b60b6ea33dd9aa
osakea <osakeatakahashi@gmail.com>
2025-07-24 19:09:31 +0700

    w

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-let question = document.querySe1ector('.question_class' )
+const question =document.querySelector('.question')
 
-let answers = document.querySe1ectorA11('nswer_class' )
-
-const num = document.querySelector('.num div')
+const num = document.querySelectorAll('.num div')
 
 class Quest{
     constructor(quest, answer, wrong1, wrong2, wrong3, wrong4){
@@ -17,12 +15,12 @@ class Quest{
         ]
     }
 
-    display() {
+    display(){
         for(let i = 0; i < this.answer_array.length; i += 1)
             {
                 num[i].innerHTML = this.answer_array[i]
         }
-        quest.innerHTML = this.quest
+        question.innerHTML = this.quest
     }
 
 
@@ -32,3 +30,16 @@ let quest1 = new Quest('5 + 3 = ?', '8', '7', '9', '10', '11')
 let quest2 = new Quest('6 x 9 = ?', '54', '50', '55', '60', '65')
 let quest3 = new Quest('9 + 3 = ?', '12', '11', '13', '14', '15')
 let quest4 = new Quest('24 : 4 = ?', '6', '5', '7', '8', '9')
+
+quest1.display()
+
+let list_quest = [quest1, quest2,quest3,quest4]
+
+let current = 0
+
+for(let i = 0; i < num.length; i += 1) {
+    num[i].addEventListener('click', function() {
+        list_quest[current].display()
+        current ++;
+    })
+}
